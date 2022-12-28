2424cd31a90b6a2d00a7f625232033c4c9e7d964
Linus Casassa <linus.casassa@r2-factory.com>
2022-12-28 09:46:57 +0000

    new structure of DB

--- toto_client/toto_client.py
+++ toto_client/toto_client.py
@@ -2,7 +2,7 @@ import json
 import os
 import time
 from io import StringIO
-from typing import Dict, List
+from typing import Optional, Dict, List
 import base64
 import requests
 import pandas as pd
@@ -34,9 +34,9 @@ class TotoClient:
         else:
             self.r2_token = "no_token"
 
-    def upload_document(self, file_path: str):
+    def upload_file(self, file_path: str):
         file_name = os.path.basename(file_path)
-        document_uuid = self.generate_document_uuid(file_path)
+        file_uuid = self.generate_file_uuid(file_path)
         with open(file_path, "rb") as file:
             file_content_base64 = base64.b64encode(file.read()).decode("utf-8")
 
@@ -49,68 +49,94 @@ class TotoClient:
         if file_path.endswith(".tif") or file_path.endswith(".tiff"):
             file_content_base64 = "data:image/tiff;base64," + file_content_base64
 
+        values = {'fileContentBase64': file_content_base64, 'fileName': file_name, 'uuid': file_uuid}
 
-        values = {'fileContentBase64': file_content_base64, 'fileName': file_name, 'fileId': document_uuid}
-
-        r = requests.post(f"{self.host}/upload_doc", json=values)
+        r = requests.post(f"{self.host}/upload_file", json=values)
         if r.status_code != 200:
             raise ValueError(f"Failed uploading {r.status_code} {r.text}")
-        return document_uuid
+        return r.json()['data_id']
 
-    def generate_document_uuid(self, file_path):
+    def generate_file_uuid(self, file_path):
         file_name = os.path.basename(file_path)
         file_size = os.path.getsize(file_path)
         file_last_modified = os.path.getmtime(file_path)
-        document_uuid = f"{file_name}-{file_size}-{file_last_modified}"
-        return document_uuid
+        file_uuid = f"{file_name}-{file_size}-{file_last_modified}"
+        return file_uuid
 
-    def queue_job(self, job_name: str, job_arguments: Dict, force=False):
-        values = {"jobName": job_name, "inputArguments": json.dumps(job_arguments)}
+    def queue_job(self, job_name: str, data_id: str, extra_arguments: Optional[Dict] = None, force=False):
+        values = {"jobName": job_name, "dataId": data_id}
+        if extra_arguments is not None:
+            values["extraArguments"] = json.dumps(extra_arguments)
         if force:
             values["force"] = "True"
 
         r = requests.get(f"{self.host}/queue_job", params=values)
         if r.status_code != 200:
             raise ValueError(f"Failed queuing job {r.status_code} {r.text}")
-        return r.json()["job_identifier"]
+        return r.json()["job_id"]
 
-    def jobs(self, job_identifiers=None):
+    def jobs(self, job_ids=None):
         values = None
-        if job_identifiers is not None:
-            values = {"jobIdentifiers": job_identifiers}
+        if job_ids is not None:
+            values = {"jobIds": job_ids}
         r = requests.get(f"{self.host}/jobs", json=values)
         if r.status_code != 200:
             raise ValueError(f"Failed querying for jobs {r.status_code} {r.text}")
         return r.json()
 
-    def wait_for_jobs_to_complete(self, job_identifiers: List[str], timeout: int = None, debug_prints: bool = False):
+    def wait_for_jobs_to_complete(self, job_ids: List[str], timeout: int = None, debug_prints: bool = False):
         if timeout is not None:
             raise NotImplementedError("timeout not implemented")
-        job_identifiers = job_identifiers.copy()
+        job_ids = job_ids.copy()
         while True:
             if debug_prints:
                 print(".", end="", flush=True)
-            jobs = self.jobs(job_identifiers)
-            for job_identifier_index in range(len(job_identifiers)):
-                job_identifier = job_identifiers[job_identifier_index]
-                if jobs[job_identifier]["status"] != "Running":
-                    job_identifiers.pop(job_identifier_index)
-            if len(job_identifiers) == 0:
+            jobs = self.jobs(job_ids)
+            for job_ids_index in range(len(job_ids)):
+                job_id = job_ids[job_ids_index]
+                if jobs[job_id]["status"] != "Running":
+                    job_ids.pop(job_ids_index)
+            if len(job_ids) == 0:
                 break
             time.sleep(1)
 
-    def get_document(self, document_id):
+    def get_data(self, data_id, tags=None, jobs=None):
+        query = ""
+        if tags is not None:
+            if isinstance(tags, str):
+                tags = [tags]
+            for tag in tags:
+                query += """
+                    %s: datas(tagName: "%s") {
+                      id
+                      dataType
+                      pageNumber
+                      polygonParent
+                      tableCsv
+                    }
+                """ % (tag,tag)
+        if jobs is not None:
+            if isinstance(jobs, str):
+                jobs = [jobs]
+            for job in jobs:
+                query += """
+                    %s: datas(jobName: "%s") {
+                      id
+                      dataType
+                      pageNumber
+                      polygonParent
+                      tableCsv
+                    }
+                """ % (job,job)
+
         query = """query {
-                      document(documentId:"%s") {
+                      data(dataId:"%s") {
                         id
-                        fileId
-                        areas {
-                          id
-                          properties
-                        }
+                        dataType
+                        %s
                       }
                     }
-                """ % (document_id,)
+                """ % (data_id, query)
         data = {"query": query, "variables": None}
         headers = {
             'Authorization': f"Bearer {self.r2_token}",
@@ -120,44 +146,24 @@ class TotoClient:
         r = requests.post(f"{self.host}/graphql", headers=headers, json=data)
         if not (200 <= r.status_code < 300):
             raise ConnectionError(r.text)
-        return r.json()['data']['document']
+        return r.json()['data']['data']
 
-    def get_area(self, area_id):
-        query = """query {
-                     area(areaId: "%s") {
-                       id
-                       properties
-                       areas {
-                         id
-                         properties
-                         polygonParent
-                       }
-                     }
-                   }
-                """ % (area_id,)
-        data = {"query": query, "variables": None}
-        headers = {
-            'Authorization': f"Bearer {self.r2_token}",
-            'Content-type': 'application/json',
-            'Accept': 'application/json'
-        }
-        r = requests.post(f"{self.host}/graphql", headers=headers, json=data)
-        if not (200 <= r.status_code < 300):
-            raise ConnectionError(r.text)
-        return r.json()['data']['area']
-
-    def detect_table(self, area_id):
-        job_identifier = self.queue_job("pageimg2tablebox_base64", {"area_id": area_id})
+    def detect_table(self, data_id):
+        data = self.get_data(data_id, jobs=["pageimg2tablebox_base64"])
+        assert data["dataType"] == "image"
+        job_identifier = self.queue_job("pageimg2tablebox_base64", data_id=data_id)
         self.wait_for_jobs_to_complete([job_identifier])
-        area = self.get_area(area_id)
-        return area["areas"]
+        data = self.get_data(data_id, jobs=["pageimg2tablebox_base64"])
+        return data["pageimg2tablebox_base64"]
 
-    def extract_table(self, area_id):
-        job_identifier = self.queue_job("hf_recognise_table_base64", {"area_id": area_id})
+    def extract_table(self, data_id):
+        data = self.get_data(data_id, jobs=["hf_recognise_table_base64"])
+        assert data["dataType"] == "image"
+        job_identifier = self.queue_job("hf_recognise_table_base64", data_id=data_id)
         self.wait_for_jobs_to_complete([job_identifier])
 
-        area = self.get_area(area_id)
-        csvString = json.loads(area["properties"])["table"]
+        data = self.get_data(data_id, jobs=["hf_recognise_table_base64"])
+        csvString = data["hf_recognise_table_base64"][0]["tableCsv"]
         csvStringIO = StringIO(csvString)
         df = pd.read_csv(csvStringIO, sep=",", header=None)
         return df
